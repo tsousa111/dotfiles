f279ad5547480a8ba2cb69c7042cb455c79a392e
tiago <tiagao2001@hotmail.com>
2024-01-27 18:53:08 +0000

    added copilot

--- .config/nvim/lua/tsousa/plugins/init.lua
+++ .config/nvim/lua/tsousa/plugins/init.lua
@@ -1,9 +1,7 @@
 return {
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
-    "nvim-lua/popup.nvim",
     "tpope/vim-surround",
-    "nvim-lualine/lualine.nvim",
-    "nvim-treesitter/playground",
     "j-hui/fidget.nvim",
+    "github/copilot.vim",
 }
