dea3bfab4410081a51d75e602276d1a6701574cf
tiago <tiagao2001@hotmail.com>
2024-01-29 01:09:55 +0000

    changed some keymaps

--- .config/nvim/lua/tsousa/plugins/lsp.lua
+++ .config/nvim/lua/tsousa/plugins/lsp.lua
@@ -218,15 +218,6 @@ return {
                 automatic_installation = true
             })
 
-
-            local opts = { noremap = true, silent = true }
-            vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-            vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-            vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-
-            -- Use an on_attach function to only map the following keys
-            -- after the language server attaches to the current buffer
             local on_attach = function(client, bufnr)
                 -- Enable completion triggered by <c-x><c-o>
                 vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
@@ -236,21 +227,16 @@ return {
                 local bufopts = { noremap = true, silent = true, buffer = bufnr }
                 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
                 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-                vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
                 vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-                -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-                vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-                vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-                vim.keymap.set('n', '<space>wl', function()
-                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-                end, bufopts)
-                vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-                vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-                vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+                vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+                vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
+                vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+                vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
                 vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-                vim.keymap.set('n', '<space>ge', function() vim.diagnostic.goto_next() end, bufopts)
-                vim.keymap.set('n', '<space>gE', function() vim.diagnostic.goto_prev() end, bufopts)
-                vim.keymap.set('n', '<space>fo', function() vim.lsp.buf.format { async = true } end, bufopts)
+                vim.keymap.set('n', '<leader>vd', vim.diagnostic.open_float, bufopts)
+                vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, bufopts)
+                vim.keymap.set('n', ']d', vim.diagnostic.goto_next, bufopts)
+                vim.keymap.set('n', '<leader>fo', function() vim.lsp.buf.format { async = true } end, bufopts)
                 vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, bufopts)
             end
 
@@ -265,7 +251,7 @@ return {
                 require('cmp_nvim_lsp').default_capabilities()
             )
 
-            mason_lspconfig.setup_handlers {
+            mason_lspconfig.setup_handlers({
 
                 -- This is a default handler that will be called for each installed server (also for new servers that are installed during a session)
                 function(server_name)
@@ -276,7 +262,7 @@ return {
                         capabilities = capabilities,
                     }
                 end,
-            }
+            })
         end
     },
 }
