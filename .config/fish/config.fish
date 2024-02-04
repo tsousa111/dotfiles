86fc4cc3b3adee7432e09fa3978ec91e0ae0efb6
tiago <tiagao2001@hotmail.com>
2024-02-04 18:22:38 +0000

    changed rust path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -28,7 +28,7 @@ bind \cf $XDG_DATA_HOME/scripts/tmux-sessionizer.sh
 
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/tsousa/.ghcup/bin $PATH # ghcup-env
 fish_add_path $HOME/.local/share/nvim/mason/bin/
-fish_add_path $HOME/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin
+fish_add_path $HOME/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin
 fish_add_path $HOME/.cargo/bin/
 fish_add_path $HOME/.config/emacs/bin
 # google cloud bin
