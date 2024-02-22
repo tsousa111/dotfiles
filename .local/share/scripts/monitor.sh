0bd09da3cd5cf89a9a4ccd2b7e9ee118ec6b0194
tiago <tiagao2001@hotmail.com>
2024-02-22 13:39:09 +0000

    check if envycontrol exists

--- .local/share/scripts/monitor.sh
+++ .local/share/scripts/monitor.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-gpu_mode=$(/usr/bin/envycontrol -q)
-if [ "$gpu_mode" = "nvidia" ]; then
-    xrandr --setprovideroutputsource modesetting NVIDIA-0
-    xrandr --auto
-    xrandr --output HDMI-0 --above eDP-1-1
-    xrandr --output HDMI-0 --primary
-else
-    xrandr --auto
-    xrandr --output HDMI-1-0 --above eDP-1
-    xrandr --output HDMI-1-0 --primary
+if [ -f /usr/bin/envycontrol ]; then
+	gpu_mode=$(/usr/bin/envycontrol -q)
+	if [ "$gpu_mode" = "nvidia" ]; then
+		xrandr --setprovideroutputsource modesetting NVIDIA-0
+		xrandr --auto
+		xrandr --output HDMI-0 --above eDP-1-1
+		xrandr --output HDMI-0 --primary
+	else
+		xrandr --auto
+		xrandr --output HDMI-1-0 --above eDP-1
+		xrandr --output HDMI-1-0 --primary
+	fi
 fi
 
 pkill picom
