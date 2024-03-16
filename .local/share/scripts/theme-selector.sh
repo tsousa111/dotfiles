7aea1d23b374352a5bec5bf4d6ed57ab5fba5377
tiago <tiagao2001@hotmail.com>
2024-03-16 23:20:36 +0000

    dunst theme selector

--- .local/share/scripts/theme-selector.sh
+++ .local/share/scripts/theme-selector.sh
@@ -16,7 +16,7 @@ if [[ $chosen == "" ]]; then
 fi
 theme=$(echo "${chosen}" | rev | cut -d- -f2- | rev)
 
-background=$(echo "${chosen}" | awk -F'-' '{print $NF}')
+tmux_bg=$(echo "${chosen}" | awk -F'-' '{print $NF}')
 
 # change rofi theme
 rofi_conf=~/.config/rofi/config.rasi
@@ -25,26 +25,37 @@ if [[ -f $rofi_theme_check ]]; then
     echo "Rofi Theme ${chosen} not found"
     exit 1
 fi
-sed -i "s/\(@import \"~\/.config\/rofi\/colors\/\).*/\1${theme}-${background}.rasi\"/" $rofi_conf
+sed -i "s/\(@import \"~\/.config\/rofi\/colors\/\).*/\1${theme}-${tmux_bg}.rasi\"/" $rofi_conf
 
 # change neovim theme
 sed -i "s/\(local color = \).*/\1\"${theme}\"/" ~/.config/nvim/lua/tsousa/plugins/colorscheme.lua
-sed -i "s/\(local background = \).*/\1\"${background}\"/" ~/.config/nvim/lua/tsousa/plugins/colorscheme.lua
+sed -i "s/\(local background = \).*/\1\"${tmux_bg}\"/" ~/.config/nvim/lua/tsousa/plugins/colorscheme.lua
 
 # change xresources
 xresources_themes=".Xresources.d"
-sed -i "s/#include \".*\"/#include \"${xresources_themes}\/${theme}-${background}\"/" ~/.Xresources
+sed -i "s/#include \".*\"/#include \"${xresources_themes}\/${theme}-${tmux_bg}\"/" ~/.Xresources
 xrdb ~/.Xresources
 
 # change tmux
 # add case for when tmux is not running
-tmux_background=$(grep "^*.background" ~/"${xresources_themes}"/"${theme}"-"${background}" | awk '{print $2}')
-tmux_foreground=$(grep "^*.foreground" ~/"${xresources_themes}"/"${theme}"-"${background}" | awk '{print $2}')
-sed -i "s/set -g status-style 'bg=.* fg=.*'/set -g status-style 'bg=${tmux_background} fg=${tmux_foreground}'/" ~/.config/tmux/tmux.conf
+tmux_bg=$(xrdb -get background)
+tmux_fg=$(xrdb -get foreground)
+sed -i "s/set -g status-style 'bg=.* fg=.*'/set -g status-style 'bg=${tmux_bg} fg=${tmux_fg}'/" ~/.config/tmux/tmux.conf
 tmux source "$HOME/.config/tmux/tmux.conf"
 
 # change dunst
-# NOTE: need to change fish theme for gruvbox
+dunst_template=~/.config/dunst/template
+dunst_conf=~/.config/dunst/dunstrc
+dunst_bg=$(xrdb -get background)
+dunst_fg=$(xrdb -get foreground)
+dunst_red=$(xrdb -get color1)
+cp "${dunst_template}" "${dunst_conf}"
+sed -i "s/\${bg}/${dunst_bg}/" "${dunst_conf}"
+sed -i "s/\${fg}/${dunst_fg}/" "${dunst_conf}"
+sed -i "s/\${color1}/${dunst_red}/" "${dunst_conf}"
+systemctl --user restart dunst.service
+
+# NOTE: need to update fish theme for gruvbox
 
 # change fish
 fish -c "echo 'y' | fish_config theme save '${chosen}'"
