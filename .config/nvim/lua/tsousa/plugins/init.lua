9fbb955000365a4f4a98f8ecca575fb3a36db93f
tsousa111 <tiagao2001@hotmail.com>
2024-10-31 00:05:47 +0000

    cloak config and starship icon fix

--- .config/nvim/lua/tsousa/plugins/init.lua
+++ .config/nvim/lua/tsousa/plugins/init.lua
@@ -10,13 +10,34 @@ return {
 	"nvim-lua/plenary.nvim",
 	"tpope/vim-surround",
 	"tpope/vim-repeat",
-	"laytan/cloak.nvim",
+	{
+		"laytan/cloak.nvim",
+		config = function()
+			require("cloak").setup({
+				enabled = true,
+				cloak_character = "*",
+				highlight_group = "Comment",
+				cloak_length = nil, -- Provide a number if you want to hide the true length of the value.
+				try_all_patterns = true,
+				cloak_telescope = true,
+				cloak_on_leave = false,
+				patterns = {
+					{
+						file_pattern = ".env*",
+						cloak_pattern = "=.+",
+						replace = nil,
+					},
+				},
+			})
+		end,
+	},
 	{
 		"ThePrimeagen/vim-be-good",
-        disabled = false,
+		disabled = false,
 	},
 	{
 		"jbyuki/instant.nvim",
+		disabled = true,
 		config = function()
 			vim.g.instant_username = "tsousa"
 		end,
